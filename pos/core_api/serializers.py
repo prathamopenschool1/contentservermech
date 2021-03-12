222d958332096f96e9820e0ded205d22be02e1c2
pratham <prathamopenschool1@pratham.org>
2021-03-12 18:14:03 +0530

    removed data,filtername,tablename, adeed uploaeed_file for FileUpload

--- pos/core_api/serializers.py
+++ pos/core_api/serializers.py
@@ -12,16 +12,23 @@ class UsageDataSerializer(serializers.ModelSerializer):
     def __init__(self, *args, **kwargs):
         many = kwargs.pop('many', True)
         super(UsageDataSerializer, self).__init__(many=many, *args, **kwargs)
-        
-    data = serializers.JSONField(default='\{\}')
-    filter_name = serializers.CharField(default='enter filter')
-    table_name = serializers.CharField(default='enter name')
+
+    #commneting data,filtername , tablename and adeed uploaeed_file for FileUpload in UsageAPI      
+    #data = serializers.JSONField(default='\{\}')
+    #filter_name = serializers.CharField(default='enter filter')
+    #table_name = serializers.CharField(default='enter name')
     created_at = serializers.DateTimeField(read_only=True)
     
+    # removing filter name and tablename
+    #class Meta: # this is original one
+    #    model = UsageData
+    #    fields = (
+	#		'id', 'data', 'filter_name', 'table_name', 'created_at',
+    #        )
     class Meta:
         model = UsageData
         fields = (
-			'id', 'data', 'filter_name', 'table_name', 'created_at',
+			'id','created_at','uploaded_file',
             )
 
 
